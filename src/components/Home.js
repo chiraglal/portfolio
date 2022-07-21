6b66fc0450bc682f8ee31783d7c1641f87e50905
CHIRAG LAL <chiraglal15@gmail.com>
2022-07-21 22:22:46 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 const Home = () => {
   const handleClick = () => {};
   return (
@@ -17,15 +18,17 @@ const Home = () => {
           Technology, passionate about programming and technology.
         </p>
         <div>
-          <button
-            className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded-md"
-            onClick={handleClick}
-          >
-            View Projects
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-          </button>
+          <Link to="projects">
+            <button
+              className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded-md"
+              onClick={handleClick}
+            >
+              View Projects
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
