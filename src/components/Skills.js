95679e801ed68099de182d70ab72a6d398f015de
CHIRAG LAL <chiraglal15@gmail.com>
2023-11-16 13:31:31 +0530

    minor changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,6 +7,9 @@ import Tailwind from "../assets/tailwind.png";
 import GitHub from "../assets/github.png";
 import Bootstrap from "../assets/bootstrap.png";
 import Material from "../assets/material.png";
+import Python from "../assets/python.png";
+import SQL from "../assets/sql.png";
+import Java from "../assets/java.png";
 const Skills = () => {
   return (
     <div name="skills" className="bg-[#0a192f] text-gray-300 w-full h-screen">
@@ -54,6 +57,18 @@ const Skills = () => {
             <div className="shadow-md hover:scale-110 duration-500 shadow-[#040c16]">
               <img src={Material} alt="HTML Icon" className="w-20 mx-auto" />
               <p>Material UI</p>
+            </div>
+            <div className="shadow-md hover:scale-110 duration-500 shadow-[#040c16]">
+              <img src={Java} alt="HTML Icon" className="w-20 mx-auto" />
+              <p>Java</p>
+            </div>
+            <div className="shadow-md hover:scale-110 duration-500 shadow-[#040c16]">
+              <img src={Python} alt="HTML Icon" className="w-20 mx-auto" />
+              <p>Python</p>
+            </div>
+            <div className="shadow-md hover:scale-110 duration-500 shadow-[#040c16]">
+              <img src={SQL} alt="HTML Icon" className="w-20 mx-auto" />
+              <p>SQL</p>
             </div>
           </div>
         </div>
