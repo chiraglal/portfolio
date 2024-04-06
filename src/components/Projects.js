91f6fbc443915db89cfdfe348d30385acaaaf11a
CHIRAG LAL <chiraglal15@gmail.com>
2024-04-06 18:50:27 +0530

    minor changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import Weatherapp from "../assets/weatherapp.png";
 import Loginpage from "../assets/loginpage.png";
 import Notes from "../assets/notes.png";
-import Quotes from "../assets/quotes.png";
 import Todos from "../assets/todos.png";
 import FoodWebsite from "../assets/foodwebsite.png";
+import dashboard from "../assets/dashboard.png";
 const Work = () => {
   return (
     <div
@@ -23,18 +23,18 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 font-mono">
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${Weatherapp})` }}
+            style={{ backgroundImage: `url(${dashboard})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div"
           >
             <div>
               {/* Hover Effects */}
-              <div className="opacity-0 group-hover:opacity-100">
+              <div className="opacity-0 group-hover:opacity-100 ease-in-out duration-300">
                 <span className="text-2xl font-bold text-white tracking-wider">
-                  Weather App | React JS App
+                  Admin Dashboard | React JS +Tailwind CSS App
                 </span>
                 <div className="pt-8 text-center">
                   <a
-                    href="https://weatherapp-reactjs-owapi.netlify.app/"
+                    href="https://react-admin-dashboard-lemon-omega.vercel.app/"
                     rel="noreferrer"
                     target="_blank"
                   >
@@ -43,7 +43,7 @@ const Work = () => {
                     </button>
                   </a>
                   <a
-                    href="https://github.com/chiraglal/Weather-App-React-JS"
+                    href="https://github.com/chiraglal/react-admin-dashboard"
                     rel="noreferrer"
                     target="_blank"
                   >
@@ -62,7 +62,7 @@ const Work = () => {
           >
             <div>
               {/* Hover Effects */}
-              <div className="opacity-0 group-hover:opacity-100">
+              <div className="opacity-0 group-hover:opacity-100 ease-in-out duration-300">
                 <span className="text-2xl font-bold text-white tracking-wider p-auto">
                   Food Website | React JS + Tailwind App
                 </span>
@@ -95,7 +95,7 @@ const Work = () => {
           >
             <div>
               {/* Hover Effects */}
-              <div className="opacity-0 group-hover:opacity-100">
+              <div className="opacity-0 group-hover:opacity-100 ease-in-out duration-300">
                 <span className="text-2xl font-bold text-white tracking-wider">
                   Front End Login Page | React JS + Tailwind App
                 </span>
@@ -123,12 +123,45 @@ const Work = () => {
             </div>
           </div>
           <div
+            style={{ backgroundImage: `url(${Weatherapp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div"
+          >
+            <div>
+              {/* Hover Effects */}
+              <div className="opacity-0 group-hover:opacity-100 ease-in-out duration-300">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Weather App | React JS App
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://weatherapp-reactjs-owapi.netlify.app/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/chiraglal/Weather-App-React-JS"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
             style={{ backgroundImage: `url(${Notes})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div"
           >
             <div>
               {/* Hover Effects */}
-              <div className="opacity-0 group-hover:opacity-100">
+              <div className="opacity-0 group-hover:opacity-100 ease-in-out duration-300">
                 <span className="text-2xl font-bold text-white tracking-wider">
                   Notes App | HTML CSS JS App
                 </span>
@@ -156,45 +189,12 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${Quotes})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div"
-          >
-            <div>
-              {/* Hover Effects */}
-              <div className="opacity-0 group-hover:opacity-100">
-                <span className="text-2xl font-bold text-white tracking-wider">
-                  Random Quote Generator | React JS App
-                </span>
-                <div className="pt-8 text-center">
-                  <a
-                    href="https://advice-quote-generator-app.netlify.app/"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                      Demo
-                    </button>
-                  </a>
-                  <a
-                    href="https://github.com/chiraglal/Web-Dev-Projects/tree/main/random-quote-generator"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                      Code
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
             style={{ backgroundImage: `url(${Todos})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div"
           >
             <div>
               {/* Hover Effects */}
-              <div className="opacity-0 group-hover:opacity-100">
+              <div className="opacity-0 group-hover:opacity-100 ease-in-out duration-300">
                 <span className="text-xl font-bold text-white tracking-wider p-auto">
                   Todos Made with Bootstrap
                 </span>
